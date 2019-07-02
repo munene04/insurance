632bc957661a76f1ffaa168da71e1b91f75dee68
Evans Munene <muneneevans04@gmail.com>
2019-07-02 12:56:07 +0300

    Added circular menu

--- app/src/main/java/android/example/insuranceapp/ActivityOne.java
+++ app/src/main/java/android/example/insuranceapp/ActivityOne.java
@@ -1,8 +1,12 @@
 package android.example.insuranceapp;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
+
+import com.imangazaliev.circlemenu.CircleMenu;
+import com.imangazaliev.circlemenu.CircleMenuButton;
 
 public class ActivityOne extends AppCompatActivity {
 
@@ -11,11 +15,50 @@ public class ActivityOne extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_one);
 
-        TextView txtInfo = (TextView)findViewById(R.id.txtInfo);
-        if(getIntent() != null)
-        {
-            String info = getIntent().getStringExtra("info");
-            txtInfo.setText(info);
-        }
+        CircleMenu circleMenu = findViewById(R.id.circleMenu);
+        circleMenu.setOnItemClickListener(new CircleMenu.OnItemClickListener() {
+            @Override
+            public void onItemClick(CircleMenuButton menuButton) {
+
+            }
+        });
+        circleMenu.setEventListener(new CircleMenu.EventListener() {
+            @Override
+            public void onMenuOpenAnimationStart() {
+
+            }
+
+            @Override
+            public void onMenuOpenAnimationEnd() {
+
+            }
+
+            @Override
+            public void onMenuCloseAnimationStart() {
+
+            }
+
+            @Override
+            public void onMenuCloseAnimationEnd() {
+
+            }
+
+            @Override
+            public void onButtonClickAnimationStart(@NonNull CircleMenuButton menuButton) {
+
+            }
+
+            @Override
+            public void onButtonClickAnimationEnd(@NonNull CircleMenuButton menuButton) {
+
+            }
+        });
+
+//        TextView txtInfo = (TextView)findViewById(R.id.txtInfo);
+//        if(getIntent() != null)
+//        {
+//            String info = getIntent().getStringExtra("info");
+//            txtInfo.setText(info);
+//        }
     }
 }
