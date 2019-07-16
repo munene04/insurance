e44665885b4ca795525518b4f714df939b1daa00
Preet <preet.tailor@strathmore.edu>
2019-07-16 16:39:42 +0300

    Updated Grid view

--- app/src/main/java/android/example/insuranceapp/MainActivity.java
+++ app/src/main/java/android/example/insuranceapp/MainActivity.java
@@ -10,34 +10,34 @@ import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.content_main);
-//    }
-
-    RecyclerViewAdapter adapter;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.content_main);
+    }
 
-        // data to populate the RecyclerView with
-        String[] data = {"Insurance", "Claim", "Policy", "Quote", "32", "41", "42", "43", "44", "45", "46", "47", "48"};
-
-        // set up the RecyclerView
-        RecyclerView recyclerView = findViewById(R.id.rvNumbers);
-        int numberOfColumns = 6;
-        recyclerView.setLayoutManager(new GridLayoutManager(this, numberOfColumns));
-        adapter = new RecyclerViewAdapter(this, data);
-        recyclerView.setAdapter(adapter);
+//    RecyclerViewAdapter adapter;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//
+//        // data to populate the RecyclerView with
+//        String[] data = {"Insurance", "Claim", "Policy", "Quote", "32", "41", "42", "43", "44", "45", "46", "47", "48"};
+//
+//        // set up the RecyclerView
+//        RecyclerView recyclerView = findViewById(R.id.rvNumbers);
+//        int numberOfColumns = 6;
+//        recyclerView.setLayoutManager(new GridLayoutManager(this, numberOfColumns));
+//        adapter = new RecyclerViewAdapter(this, data);
+//        recyclerView.setAdapter(adapter);
 //        adapter.setClickListener(this);
-    }
-
-    public void onItemClick(View view, int position) {
-        Log.i("TAG", "You clicked number " + adapter.getItem(position) + ", which is at cell position " + position);
-    }
+//    }
+//
+//    public void onItemClick(View view, int position) {
+//        Log.i("TAG", "You clicked number " + adapter.getItem(position) + ", which is at cell position " + position);
+//    }
 }
 
 
